9bc578a84bf45c98bfca55b584b25b12fb550866
Rajat Jindal <rajatjindal83@gmail.com>
2014-07-01 22:20:10 -0700

    fix post data issue

--- cgi-bin/index.pl
+++ cgi-bin/index.pl
@@ -8,14 +8,10 @@ use Users;
 use Data::Dump qw(dump);
 
 my $q = CGI->new();
-print $q->header;
-print "Hello\n";
 my $method = lc($ENV{'REQUEST_METHOD'});
 my $path = $ENV{'REQUEST_URI'};
-$path =~ s/\/cgi-bin\/index.pl\///g;
-print "Path: '$path'\n";
-print dump($q->param('POSTDATA'));
-print "Hi hello\n";
-my $users = Users->new();
-print dump($users->get_users());
+my $data = $q->param('POSTDATA');
 
+print $q->header;
+print "Hello";
+print dump($data);
--- htdocs/bkp/bootstrap/js/bkp.js
+++ htdocs/bkp/bootstrap/js/bkp.js
@@ -3,7 +3,6 @@ $(document).ready(function(e) {
     $("form[ajax=true]").submit(function(e) {
         e.preventDefault();
         var form_data = $(this).serialize();
-        alert(form_data);
         var form_url = $(this).attr("action");
         var form_method = $(this).attr("method").toUpperCase();
         
@@ -14,6 +13,7 @@ $(document).ready(function(e) {
             type: form_method,      
             data: form_data,     
             cache: false,
+            beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
             success: function(returnhtml){
                 alert(returnhtml);
                 $("#result").html(returnhtml); 
