f4966b80f444c2e2816c731fc919dfc7f59baa03
Rajat Jindal <rajatjindal83@gmail.com>
2014-07-02 17:59:51 -0700

    redirect to dashboard if login successful

--- htdocs/bkp/bootstrap/js/bkp.js
+++ htdocs/bkp/bootstrap/js/bkp.js
@@ -19,25 +19,24 @@
 
 $(document).ready(function(e) {
     
-    $("form[ajax=true]").submit(function(e) {
+    $("#loginForm").submit(function(e) {
         e.preventDefault();
-        var form_data = $(this).serializeFormJSON();
+        var form_data = JSON.stringify({
+            "email": document.getElementById('email').value,
+            "password": document.getElementById('password').value
+        });
         var form_url = $(this).attr("action");
         var form_method = $(this).attr("method").toUpperCase();
-        
-        $("#loadingimg").show();
-        
+        alert(form_data);
         $.ajax({
             url: form_url, 
             type: form_method,      
             data: form_data,     
             cache: false,
             beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
-            success: function(returnhtml){
-                alert(returnhtml);
-                $("#result").html(returnhtml); 
-                $("#loadingimg").hide();                    
-            }           
+            success: function(returnhtml) {
+                window.location = "/bkp/dashboard.html";
+            }
         });    
         
     });
